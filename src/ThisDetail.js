9a1968f61f59ebd7df8bab39d1f063c698e09cae
NajamShehzad <najambutt195@gmail.com>
2018-08-23 23:29:54 +0500

    Changing Component from class to function

--- src/ThisDetail.js
+++ src/ThisDetail.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export class  ThisScope  extends  Component {
-    render() {
-        return(
-            <div>
-                <h1 style={{margin:5}} >{this.props.heading}</h1>
-                <p style={{margin:10}}>{this.props.detail}</p>
-            </div>
-        )
-    }
+export function ThisScope(props) {
+    return (
+        <div>
+            <h1 style={{ margin: 5 }} >{props.heading}</h1>
+            <p style={{ margin: 10 }}>{props.detail}</p>
+        </div>
+    )
+
 };
 
